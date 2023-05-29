816ea632c95c8de2308b111b1fa55df4276451dc
Prashant Agrawal <prashant.agrawal@opumtechnologies.com>
2023-05-29 20:11:50 +1200

    01 Add swagger as root page

---
+++ TresurehuntApi/Controllers/IndexController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Rtp.Web.Api.Controllers
+{
+    /// <summary>
+    ///
+    /// </summary>
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("/")]
+    [ApiController]
+    public class IndexController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="hostingEnv"></param>
+        public IndexController(IConfiguration configuration, IWebHostEnvironment hostingEnv)
+        {
+            _configuration = configuration;
+            _env = hostingEnv;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Redirect("/swagger/index.html");
+        }
+    }
+}
